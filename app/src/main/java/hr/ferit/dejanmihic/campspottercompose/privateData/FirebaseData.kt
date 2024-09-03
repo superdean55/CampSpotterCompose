908ffe27e6a05e3019ba903945d3df7ade393095
superdean55 <superdean55@gmail.com>
2024-09-03 09:37:44 +0200

    firebase data

---
+++ app/src/main/java/hr/ferit/dejanmihic/campspottercompose/privateData/FirebaseData.kt
@@ -0,0 +1,4 @@
+package hr.ferit.dejanmihic.campspottercompose.privateData
+
+const val FIREBASE_STORAGE_URL = "gs://camp-spotter-compose.appspot.com"
+const val FIREBASE_REALTIME_DB_URL = "https://camp-spotter-compose-default-rtdb.europe-west1.firebasedatabase.app"
